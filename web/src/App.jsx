61877172b76c90d5f7e9f2632218ca4a60dc988f
KeftonDavid <keftondavid@gmail.com>
2022-06-17 16:29:18 -0300

    Quinta aula: roteamento, criação de página de cadastro e alterações no layout

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -1,8 +1,17 @@
 import { Home } from "./Home";
 import { Login } from "./Login";
+import { Signup } from "./Signup";
 import { useState } from 'react';
 export function App(){
-    const [user, setUser] = useState()
+    const [user, setUser] = useState();
 
-    return user ? <Home></Home> : <Login signInUser={setUser}></Login>
+    if(user){
+        return <Home></Home>
+    }
+
+    return window.location.pathname === '/signup'
+        ? <Signup signInUser={setUser}></Signup>
+        : <Login signInUser={setUser}></Login>
 }
+
+window
